fa5c91e53a70a8938417615d947e92c3b2453a59
chav1961 <chav1961@winmain>
2020-01-12 22:16:48 +0500

    Migrate to Java 9  * <p>This class is  handler to support "root" protocol URL. Format of root URL is:</p>  * <code><b>root://</b>&lt;className&gt;/&lt;pathTooResource&gt;</code>  * <ul>  * <li>className - qualified canonical name of any class in your application. It will be used in {@linkplain Class#forName(String)} call</li>  * <li>pathToResource - absolute path to your resource from the root of your application</li>  * </ul>  * @see URLStreamHandler  * @author Alexander Chernomyrdin aka chav1961  * @since 0.0.2  * @lastUpdate 0.0.4

---
+++ src/main/java/chav1961/fsyscommander/i18n/Test.java
@@ -0,0 +1,5 @@
+package chav1961.fsyscommander.i18n;
+
+public class Test {
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module chav1961.fsyscommander {
+	requires chav1961.purelib;
+	requires java.desktop;
+	requires java.scripting;
+	requires java.xml;
+	requires java.sql;
+	requires java.rmi;
+	requires java.management;
+	requires jdk.httpserver;
+	requires java.compiler;
+	requires jdk.javadoc;
+
+	opens chav1961.fsyscommander to chav1961.purelib;
+}
